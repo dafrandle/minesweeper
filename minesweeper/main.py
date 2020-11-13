6f4a45686479134ff8042b4b40574d17600897e9
Dafrandle <dafrandle@yahoo.com>
2020-11-12 19:40:06 -0600

    Fix an error with the math that calculated the cells on the edge of the board and the cells to check when the row szie was not equal to the column size

--- minesweeper/main.py
+++ minesweeper/main.py
@@ -4,12 +4,12 @@ import random
 gameOpen = None
 
 class cellFrame():  # --------------------------------------- this is each cell that holds a button ------------------------------------------
-    def __init__(self, rowPosition, colPosition, masterFrame, window,edgeDict,rowX,bombCount):
+    def __init__(self, rowPosition, colPosition, masterFrame, window, edgeDict, columnY, bombCount):
         self.window = window
         self.rowPosition = rowPosition
         self.colPosition = colPosition
         self.edgeDict = edgeDict
-        self.rowX = rowX
+        self.columnY = columnY
         self.bombCount = bombCount
         self.frame = tk.Frame(masterFrame, width=26, height=26, highlightbackground="gray", highlightthickness=1, bg="dark grey")
         self.frame.grid(row=self.rowPosition, column=self.colPosition, padx=0, pady=0)
@@ -50,13 +50,13 @@ class cellFrame():  # --------------------------------------- this is each cell 
             self.show()
         else:
             showlist = [self.thisCellIndex]  # get cells immediately adjacent to THIS cell + itself and puts it in a list
-            for cell in adjacentCellList(self.thisCellIndex, self.edgeDict, self.rowX):
+            for cell in adjacentCellList(self.thisCellIndex, self.edgeDict, self.columnY):
                 if not self.buttonList[cell].getBombStatus() and self.buttonList[cell].getAdjacentBombs() == 0:
                     showlist.append(cell)
 
             for cell in showlist: # get adjacent cells that are not bombs nor adjacent to them and adds them to the list
                 addList = []
-                for addCell in adjacentCellList(self.buttonList[cell].getIndex(), self.edgeDict, self.rowX):
+                for addCell in adjacentCellList(self.buttonList[cell].getIndex(), self.edgeDict, self.columnY):
                     if not self.buttonList[addCell].getBombStatus() and self.buttonList[addCell].getAdjacentBombs() == 0:
                         addList.append(addCell)
                 for i in addList:
@@ -65,7 +65,7 @@ class cellFrame():  # --------------------------------------- this is each cell 
 
             addAdjCell = []
             for cell in showlist: # get adjacent cells that are not bombs BUT ARE adjacent to them and adds them to a new list
-                for addCell in adjacentCellList(self.buttonList[cell].getIndex(), self.edgeDict, self.rowX):
+                for addCell in adjacentCellList(self.buttonList[cell].getIndex(), self.edgeDict, self.columnY):
                     if not self.buttonList[addCell].getBombStatus():
                         if addCell not in showlist:
                             addAdjCell.append(addCell) # a new lits is used to prevent the program from checking added cells
@@ -146,9 +146,10 @@ def createGameWindow(rowX, columnY,numberOfBombs):
         "topRow" : [],
         "bottomRow" : []
     }
-    for i in range(rowX):
-        edgeDict["leftColumn"].append(i*rowX) # left column
-        edgeDict["rightColumn"].append(i*rowX+rowX-1) # right column
+    for i in range(rowX):  # columns
+        edgeDict["leftColumn"].append(i*columnY) # left column
+        edgeDict["rightColumn"].append(i*columnY+columnY-1) # right column
+    for i in range(columnY):  # rows
         edgeDict["topRow"].append(i) # top row
         edgeDict["bottomRow"].append((rowX*columnY-1)-i) # bottom row
 
@@ -156,7 +157,7 @@ def createGameWindow(rowX, columnY,numberOfBombs):
     buttonList = []
     for row in range(rowX):
         for column in range(columnY):
-            frameToCreate = cellFrame(row, column, gridFrame, gameWindow,edgeDict,rowX,numberOfBombs)
+            frameToCreate = cellFrame(row, column, gridFrame, gameWindow,edgeDict,columnY,numberOfBombs)
             buttonList.append(frameToCreate)
 
     # ----- give cells their index's, the list, and the count of list
@@ -164,6 +165,7 @@ def createGameWindow(rowX, columnY,numberOfBombs):
         cell.setThisCellIndex(buttonList.index(cell))
         cell.setButtonList(buttonList)
         cell.setCellCount(len(buttonList))
+        # cell.button.configure(text=buttonList.index(cell))
 
     # ------------- bomb cell list
     bombListSize = numberOfBombs
@@ -189,7 +191,7 @@ def createGameWindow(rowX, columnY,numberOfBombs):
         if cell.isBomb:
             continue
         else:
-            checkList = adjacentCellList(cellIndex,edgeDict,rowX)
+            checkList = adjacentCellList(cellIndex,edgeDict,columnY)
         for x in checkList:
             if buttonList[x].isBomb:
                 adjBombs = 1 + adjBombs
@@ -295,7 +297,7 @@ def timer():
     if gameOpen:
         root.after(1000, timer)  # call this function again in 1,000 milliseconds
 
-def adjacentCellList(cellIndex,edgeDict,rowX):
+def adjacentCellList(cellIndex,edgeDict,columnY):
     checkList = []
     sideDict = {
         "top": True,
@@ -310,25 +312,25 @@ def adjacentCellList(cellIndex,edgeDict,rowX):
         checkList.append(cellIndex + 1)
         sideDict["right"] = False
     if cellIndex not in edgeDict["topRow"]:  # check above cell
-        checkList.append(cellIndex - rowX)
+        checkList.append(cellIndex - columnY)
         sideDict["top"] = False
     if cellIndex not in edgeDict["bottomRow"]:  # check below cell
-        checkList.append(cellIndex + rowX)
+        checkList.append(cellIndex + columnY)
         sideDict["bottom"] = False
     if not sideDict["top"] and not sideDict["left"]:  # top left cell
-        checkList.append(cellIndex - rowX - 1)
+        checkList.append(cellIndex - columnY - 1)
     if not sideDict["top"] and not sideDict["right"]:  # top right cell
-        checkList.append(cellIndex - rowX + 1)
+        checkList.append(cellIndex - columnY + 1)
     if not sideDict["bottom"] and not sideDict["left"]:  # bottom left cell
-        checkList.append(cellIndex + rowX - 1)
+        checkList.append(cellIndex + columnY - 1)
     if not sideDict["bottom"] and not sideDict["right"]:  # bottom left cell
-        checkList.append(cellIndex + rowX + 1)
+        checkList.append(cellIndex + columnY + 1)
     return checkList
 
 
 # --------------------------------------------------------- Root window ------------------------------------------------
 root = tk.Tk()
-newGameButton = tk.Button(text="New game", command=lambda: createGameWindow(15, 15 , 10))
+newGameButton = tk.Button(text="New game", command=lambda: createGameWindow(16, 30, 40))   # (x,y,bombs)
 newGameButton.pack()
 
 # screen size - center widows
